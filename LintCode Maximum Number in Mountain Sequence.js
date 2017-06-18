ad3ed01a89fd4e14c47c5cb60127886b14bf1e72
KAILIN SHI <kailinshi19809@gmail.com>
2017-06-17 23:48:13 -0700

    Given a mountain sequence of n integers which increase firstly and then decrease, find the mountain top.
    Example
    Given nums = [1, 2, 4, 8, 6, 3] return 8
    Given nums = [10, 9, 8, 7], return 10

---
+++ LintCode Maximum Number in Mountain Sequence.js
@@ -0,0 +1,27 @@
+var mountainSequence = function (nums) {
+    if ( nums == null || nums.length == 0 ) {
+        return -1;
+    }
+
+    var mid;
+    var start = 0;
+    var end = nums.length - 1;
+
+    while ( start + 1 < end ) {
+        mid = start + Math.round( (end - start) / 2 );
+
+        if( nums[mid-1] < nums[mid] && nums[mid] < nums[mid+1] ) {
+            start = mid;
+        } else if ( nums[mid-1] > nums[mid] && nums[mid] > nums[mid+1] ) {
+            end = mid;
+        } else {
+            return nums[mid];
+        }
+    }
+
+    if( nums[start] < nums[end] ) {
+        return nums[end];
+    } else {
+        return nums[start];
+    }
+};
