fe6cf102418f13573f27efa93b3fb0812625392c
KAILIN SHI <kailinshi19809@gmail.com>
2017-06-18 03:42:33 -0700

    Added comments.

--- 153. Find Minimum in Rotated Sorted Array.js
+++ 153. Find Minimum in Rotated Sorted Array.js
@@ -2,6 +2,11 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+/**
+ * 不能用nums[0]作为target，因为[1,2,3] [4,5,6,7,1,2,3]
+ * 一定要用nums[end]作为target
+ */
 var findMin = function(nums) {
     if( nums == null || nums.length == 0 ) {
         return -1;
@@ -9,11 +14,12 @@ var findMin = function(nums) {
 
     var start = 0;
     var end = nums.length - 1;
+    var target = nums[end];
     var mid;
 
     while ( start + 1 < end ) {
         mid = start + Math.round( (end - start) / 2 );
-        if ( nums[mid] < nums[end] ){
+        if ( nums[mid] < target ) {
             end = mid;
         } else {
             start = mid;
