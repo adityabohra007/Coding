cbc86aefd74dac7b4a94179f637ef46b041f19c6
KAILIN SHI <kailinshi19809@gmail.com>
2017-06-17 19:15:30 -0700

    Rename the file.

---
+++ LintCode Last Position of Target.js
@@ -0,0 +1,32 @@
+var lastPosition = function (nums, target) {
+    if ( nums == null || nums.length == 0 ) {
+        return -1;
+    }
+
+    var mid;
+    var start = 0;
+    var end = nums.length - 1;
+
+    while ( start + 1 < end ) {
+        mid = start + Math.round( (end - start) / 2 );
+
+        if ( nums[mid] == target ) {
+            return mid;
+        } else if ( nums[mid] < target ) {
+            start = mid;
+        } else {
+            end = mid
+        }
+    }
+
+    // 先判断 end
+    if ( nums[end] == target ) {
+        return end;
+    }
+
+    if( nums[start] == target ) {
+        return start;
+    }
+
+    return -1;
+};
