2e9ed95e12b56d0be1732232d78cd8034fe6b70d
KAILIN SHI <kailinshi19809@gmail.com>
2017-06-25 20:33:23 -0700

    Listed how to do preOrder & inOrder & postOrder in JS.

---
+++ LintCode preOrder-inOrder-postOrder.js
@@ -0,0 +1,46 @@
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+var preOrder = function( node ) {
+    var result = [];
+
+    if ( !node ) {
+        return result;
+    }
+
+    result.push(node);
+    preOrder(node.left);
+    preOrder(node.right);
+
+    return result;
+};
+
+var inOrder = function ( node ) {
+    var result = [];
+
+    if ( !node ) {
+        return result;
+    }
+
+    inOrder(node.left);
+    result.push(left);
+    inOrder(node.left);
+
+    return result;
+};
+
+var postOrder = function ( node ) {
+    var result = [];
+
+    if ( !node ) {
+        return result;
+    }
+
+    postOrder(node.left);
+    postOrder(node.right);
+    result.push(node);
+
+    return result;
+};
