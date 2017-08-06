98f620ccb2b5b985b1c0f3ca95aafa0e184a5738
KAILIN SHI <kailinshi19809@gmail.com>
2017-08-05 20:06:56 -0700

    Added another approach.

--- 104. Maximum Depth of Binary Tree.js
+++ 104. Maximum Depth of Binary Tree.js
@@ -19,3 +19,21 @@ var maxDepth = function(root) {
 
     return Math.max( left, right ) + 1;
 };
+
+// 另一种写法
+var maxDepthII = function(root) {
+    return helper ( root, 0 );
+
+    function helper( root, depth ) {
+        if ( !root ){
+            return depth;
+        }
+
+        depth += 1;
+
+        var leftPart = helper( root.left, depth );
+        var rightPart = helper( root.right, depth );
+
+        return Math.max( leftPart, rightPart );
+    }
+};
