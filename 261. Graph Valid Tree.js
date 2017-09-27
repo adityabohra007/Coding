37b5149e0a82997da17b03b01955ff05fd49a274
KAILIN SHI <kailinshi19809@gmail.com>
2017-09-26 22:58:12 -0700

    Optimized the 'graph valid tree' function.

--- 261. Graph Valid Tree.js
+++ 261. Graph Valid Tree.js
@@ -4,49 +4,39 @@
  * @return {boolean}
  */
 var validTree = function(n, edges) {
-    if ( n == 0 ) {
+    if ( edges.length + 1 != n ) {
         return false;
     }
 
-    if ( n != (edges.length + 1) ) {
-        return false;
-    }
-
-    // initial graph
-    var graph = [];
+    var nodes = [];
     for ( var i = 0; i < n; i++ ) {
-        graph[i] = [];
+        nodes[i] = [];
     }
 
     for ( var j = 0; j < edges.length; j++ ) {
-        var u = edges[j][0];
-        var v = edges[j][1];
-
-        graph[u].push( v );
-        graph[v].push( u );
+        nodes[edges[j][0]].push( edges[j][1] );
+        nodes[edges[j][1]].push( edges[j][0] );
     }
 
-    // validation
     var arr1 = [];
     var arr2 = [];
 
-    arr1.push(0);
-    arr2.push(0);
+    arr1.push( 0 );
+    arr2.push( 0 );
 
     while ( arr1.length > 0 ) {
         var node = arr1.shift();
-        var neighbors = graph[node];
-        if ( neighbors != -1 && neighbors.length > 0 ) {
-            for ( var i = 0; i < neighbors.length; i++ ) {
-                if ( arr2.indexOf( neighbors[i] ) != -1 ) {
+        var neighbors = nodes[node];
+        if ( neighbors.length > 0 ) {
+            for ( var k = 0; k < neighbors.length; k++ ) {
+                if ( arr2.indexOf( neighbors[k] ) != -1 ) {
                     continue;
                 }
-                arr1.push( Number(neighbors[i]) );
-                arr2.push( Number(neighbors[i]) );
+                arr1.push(neighbors[k]);
+                arr2.push(neighbors[k]);
             }
         }
-        graph[node] = -1;
     }
 
-    return arr2.length == n;
+    return arr2.length == n
 };
