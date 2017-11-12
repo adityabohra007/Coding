de446c1e68da5accee035817ccf4183c406bd110
KAILIN SHI <kailinshi19809@gmail.com>
2017-11-11 16:26:53 -0800

    Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest transformation sequence from beginWord to endWord, such that:
    Only one letter can be changed at a time.
    Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
    For example,
    
    Given:
    beginWord = "hit"
    endWord = "cog"
    wordList = ["hot","dot","dog","lot","log","cog"]
    As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
    return its length 5.
    
    Note:
    Return 0 if there is no such transformation sequence.
    All words have the same length.
    All words contain only lowercase alphabetic characters.
    You may assume no duplicates in the word list.
    You may assume beginWord and endWord are non-empty and are not the same.
    UPDATE (2017/1/20):
    The wordList parameter had been changed to a list of strings (instead of a set of strings). Please reload the code definition to get the latest changes.

---
+++ 127. Word Ladder.js
@@ -0,0 +1,73 @@
+/**
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {number}
+ */
+var ladderLength = function(beginWord, endWord, wordList) {
+    if ( wordList == null || wordList.length == 0 ) {
+        return 0;
+    }
+
+    if ( beginWord == endWord ) {
+        return 1;
+    }
+
+    var queue = [];
+    var tmp = [];
+
+    queue.push( beginWord );
+    tmp.push(beginWord);
+
+    var length = 1;
+    while ( queue.length > 0 ) {
+        length++;
+        var size = queue.length;
+        for ( var k = 0; k < size; k++ ) {
+            var word = queue.shift();
+            var nextWords = getNextWords(word, wordList);
+            if (nextWords.length > 0) {
+                for (var i = 0; i < nextWords.length; i++) {
+                    if (nextWords[i] == endWord) {
+                        return length;
+                    }
+
+                    if (tmp.indexOf(nextWords[i]) > -1) {
+                        continue;
+                    }
+
+                    queue.push(nextWords[i]);
+                    tmp.push(nextWords[i]);
+                }
+            }
+        }
+    }
+    return 0;
+
+    function getNextWords( word, wordList )
+    {
+        var nextWords = [];
+        for ( var i = 0; i < 26; i++ ) {
+            for ( var j = 0; j < word.length; j++ ) {
+                var char = String.fromCharCode( 'a'.charCodeAt(0) + i );
+                if ( char == word.charAt(j) ) {
+                    continue;
+                }
+                var nextWord = replace( word, j, char );
+                if ( wordList.indexOf( nextWord ) > -1 ) {
+                    nextWords.push( nextWord );
+                }
+            }
+        }
+
+        return nextWords;
+    }
+
+    function replace ( word, pos, char )
+    {
+        var wordTmp = word.split('');
+        wordTmp[pos] = char;
+        wordTmp = wordTmp.join('');
+        return wordTmp;
+    }
+};
