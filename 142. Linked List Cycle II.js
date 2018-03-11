71d9308d417dc4f571e4cacfa611680e07f67ed3
KAILIN SHI <kailinshi19809@gmail.com>
2018-03-10 22:15:31 -0800

    Updated 142. Linked List Cycle II.

--- 142. Linked List Cycle II.js
+++ 142. Linked List Cycle II.js
@@ -34,3 +34,42 @@ var detectCycle = function(head) {
     }
 
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycleII = function(head) {
+    if ( head == null || head.next == null ) {
+        return null;
+    }
+
+    var fast = head.next;
+    var slow = head;
+
+    while ( fast != slow ) {
+        if ( fast == null || fast.next == null ) {
+            return null;
+        }
+
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+
+    slow = head;
+
+    while ( fast.next != slow ) {
+        fast = fast.next;
+        slow = slow.next;
+    }
+
+    return slow;
+};
