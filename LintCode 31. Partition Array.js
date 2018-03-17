8e7c729be2a10258dd34fe382638aff5f0705f33
KAILIN SHI <kailinshi19809@gmail.com>
2018-03-17 02:55:45 -0700

    Given an array nums of integers and an int k, partition the array (i.e move the elements in "nums") such that:
    All elements < k are moved to the left
    All elements >= k are moved to the right
    Return the partitioning index, i.e the first index i nums[i] >= k.

--- LintCode 31. Partition Array.js
+++ LintCode 31. Partition Array.js
@@ -2,6 +2,16 @@
  * @param nums: The integer array you should partition
  * @param k: An integer
  * @return: The index after partition
+ *
+ *
+ Given an array nums of integers and an int k, partition the array (i.e move the elements in "nums") such that:
+
+ All elements < k are moved to the left
+ All elements >= k are moved to the right
+ Return the partitioning index, i.e the first index i nums[i] >= k.
+
+ Example
+ If nums = [3,2,2,1] and k=2, a valid answer is 1.
  */
 const partitionArray = function (nums, k) {
     if ( nums === null || nums.length === 0 ) {
