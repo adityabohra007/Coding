1acdba59392d13f8cf9e738fd39e99527dbbb93b
KAILIN SHI <kailinshi19809@gmail.com>
2018-03-31 17:55:24 -0700

    Given some points and a point origin in two dimensional space, find k points out of the some points which are nearest to origin. Return these points sorted by distance, if they are same with distance, sorted by x-axis, otherwise sorted by y-axis.
    Have you met this question in a real interview? Yes
    Example
    Given points = [[4,6],[4,7],[4,4],[2,5],[1,1]], origin = [0, 0], k = 3
    return [[1,1],[2,5],[4,4]]

---
+++ LintCode 612. K Closest Points.py
@@ -0,0 +1,52 @@
+"""
+Definition for a point.
+class Point:
+    def __init__(self, a=0, b=0):
+        self.x = a
+        self.y = bDefinition for a point.
+class Point:
+    def __init__(self, a=0, b=0):
+        self.x = a
+        self.y = b
+"""
+
+import heapq
+
+class Solution:
+    """
+    @param points: a list of points
+    @param origin: a point
+    @param k: An integer
+    @return: the k closest points
+    """
+
+    def kClosest(self, points, origin, k):
+        self.heap = []
+        for point in points:
+            dist = self.getDistance(point, origin)
+            heapq.heappush(self.heap, Type(dist, point))
+            if len(self.heap) > k:
+                heapq.heappop(self.heap)
+
+        ret = []
+        while len(self.heap) > 0:
+            ret.append(heapq.heappop(self.heap).point)
+
+        ret.reverse()
+        return ret
+
+    def getDistance(self, pointA, pointB):
+        return (pointA.x - pointB.x)**2 + (pointA.y - pointB.y)**2
+
+
+class Type:
+    def __init__(self, dist, point):
+        self.dist = dist
+        self.point = point
+
+    def __cmp__(self, other):
+        if other.dist != self.dist:
+            return other.dist - self.dist
+        if other.point.x != self.point.x:
+            return other.point.x - self.point.x
+        return other.point.y - self.point.y
