ee1973884cd2ca25fe9a69bd20fdce3ec638ea84
KAILIN SHI <kailinshi19809@gmail.com>
2018-04-01 01:05:09 -0700

    Added another solution for 232. Implement Queue using Stacks.

--- 232. Implement Queue using Stacks.py
+++ 232. Implement Queue using Stacks.py
@@ -1,4 +1,4 @@
-class MyQueue(object):
+class MyQueue1(object):
     def __init__(self):
         self.stack1 = []
         self.stack2 = []
@@ -24,6 +24,42 @@ class MyQueue(object):
 
     def empty(self):
         return len(self.stack1) == 0
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
+
+"""
+方法2:
+只要stack2不为空，那pop和peek操作都在stack2中进行，因为stack2已经是倒序了
+"""
+class MyQueue2(object):
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x):
+        self.stack1.append(x)
+
+    def pop(self):
+        self.helper()
+        return self.stack2.pop()
+
+    def peek(self):
+        self.helper()
+        return self.stack2[-1]
+
+    def empty(self):
+        return len(self.stack1) + len(self.stack2) == 0
+
+    def helper(self):
+        if len(self.stack2) == 0:
+            while len(self.stack1) != 0:
+                self.stack2.append(self.stack1.pop())
 
 
 # Your MyQueue object will be instantiated and called as such:
