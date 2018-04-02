5217765fd0d9e167ebcd1f5d8c8c4cd5f1d57f45
KAILIN SHI <kailinshi19809@gmail.com>
2018-04-01 18:11:34 -0700

    Given two sorted integer arrays A and B, merge B into A as one sorted array.
     Notice
    
    You may assume that A has enough space (size that is greater or equal to m + n) to hold additional elements from B. The number of elements initialized in A and B are m and n respectively.
    
    Example
    A = [1, 2, 3, empty, empty], B = [4, 5]
    
    After merge, A will be filled as [1, 2, 3, 4, 5]

---
+++ LintCode 64. Merge Sorted Array.py
@@ -0,0 +1,28 @@
+class Solution:
+    """
+    @param: A: sorted integer array A which has m elements, but size of A is m+n
+    @param: m: An integer
+    @param: B: sorted integer array B which has n elements
+    @param: n: An integer
+    @return: nothing
+    """
+
+    def mergeSortedArray(self, A, m, B, n):
+
+        m = m - 1
+        n = n - 1
+        l = m + n + 1
+
+        while m >= 0 and n >= 0:
+            if A[m] > B[n]:
+                A[l] = A[m]
+                m, l = m - 1, l - 1
+            else:
+                A[l] = B[n]
+                n, l = n - 1, l - 1
+
+        while n >= 0:
+            A[l] = B[n]
+            n, l = n - 1, l - 1
+
+        return A
