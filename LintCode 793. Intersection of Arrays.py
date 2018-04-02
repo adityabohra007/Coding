8ae0bac54bdc97c2ef4710d4cd63f8586c07fb43
KAILIN SHI <kailinshi19809@gmail.com>
2018-04-02 01:23:36 -0700

    Updated an another solution.

--- LintCode 793. Intersection of Arrays.py
+++ LintCode 793. Intersection of Arrays.py
@@ -17,3 +17,28 @@ class Solution:
         right = self.helper(arrs[len(arrs) / 2:])
 
         return list(set(left) & set(right))
+
+"""
+Solution2 没有验证
+如果有一个arr里有重复的值，答案不准确
+"""
+class Solution2:
+    """
+    @param arrs: the arrays
+    @return: the number of the intersection of the arrays
+    """
+
+    def intersectionOfArrays(self, arrs):
+        #
+        M = {}
+        for i in arrs:
+            for j in i:
+                if j in M:
+                    M[j] = M[j] + 1
+                else:
+                    M[j] = 1
+        ans = 0
+        for k, v in M.iteritems():
+            if v == len(arrs):
+                ans += 1
+        return ans
