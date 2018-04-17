08443606ee191b566af2d918be631dddf2b49e63
KAILIN SHI <kailinshi19809@gmail.com>
2018-04-17 00:14:01 -0700

    Another solution to fast power

--- LintCode 140. Fast Power.py
+++ LintCode 140. Fast Power.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution_1:
     """
     @param a: A 32bit integer
     @param b: A 32bit integer
@@ -17,3 +17,30 @@ class Solution:
             n = n >> 1
 
         return ret % b
+
+"""
+Solution 2
+"""
+
+
+class Solution_2:
+    """
+    @param a: A 32bit integer
+    @param b: A 32bit integer
+    @param n: A 32bit integer
+    @return: An integer
+    """
+
+    def fastPower(self, a, b, n):
+
+        if n == 0:
+            return 1 % b
+        elif n == 1:
+            return a % b
+        else:
+            value = self.fastPower(a, b, n / 2)
+
+            if n & 1:
+                return (value * value * a) % b
+            else:
+                return (value * value) % b
