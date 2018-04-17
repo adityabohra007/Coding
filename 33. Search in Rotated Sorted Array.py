5b4babc7b60aeaa31d34d24074af24154a8f9cf8
KAILIN SHI <kailinshi19809@gmail.com>
2018-04-17 02:36:53 -0700

    Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
    (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
    
    You are given a target value to search. If found in the array return its index, otherwise return -1.
    
    You may assume no duplicate exists in the array.
    
    Your algorithm's runtime complexity must be in the order of O(log n).
    
    Example 1:
    
    Input: nums = [4,5,6,7,0,1,2], target = 0
    Output: 4
    Example 2:
    
    Input: nums = [4,5,6,7,0,1,2], target = 3
    Output: -1

---
+++ 33. Search in Rotated Sorted Array.py
@@ -0,0 +1,36 @@
+"""
+分2段讨论
+"""
+
+
+
+class Solution(object):
+    def search(self, nums, target):
+        if not nums or len(nums) == 0 or target is None:
+            return -1
+
+        low = 0
+        high = len(nums) - 1
+        inLeftHalf = target >= nums[0]
+
+        while low + 1 < high:
+            mid = (low + high) / 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < nums[high]:
+                if nums[mid] < target and target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+            else:
+                if nums[mid] > target and target >= nums[low]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+        if nums[low] == target:
+            return low
+        elif nums[high] == target:
+            return high
+        else:
+            return -1
