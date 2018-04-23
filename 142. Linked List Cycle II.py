c6f714f50f267a0afed90d3a47a895e9af0d66f6
KAILIN SHI <kailinshi19809@gmail.com>
2018-04-23 01:42:13 -0700

    Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
    Note: Do not modify the linked list.
    
    Follow up:
    Can you solve it without using extra space?

---
+++ 142. Linked List Cycle II.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution(object):
+    def detectCycle(self, head):
+        if head is None or head.next is None:
+            return None
+
+        slow = head
+        fast = head
+
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                slow = head
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+                return slow
+        return None
