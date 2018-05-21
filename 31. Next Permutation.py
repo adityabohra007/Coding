8b27450b033701c710aa1417d339497fb46bfe18
KAILIN SHI <kailinshi19809@gmail.com>
2018-05-20 23:47:02 -0700

    31. Next Permutation

--- 31. Next Permutation.py
+++ 31. Next Permutation.py
@@ -70,3 +70,22 @@ class Solution(object):
                 right -= 1
             self.quickSort(A, left, end)
             self.quickSort(A, start, right)
+
+
+"""
+Solution 2: 因为是递减的，所以不需要用快排
+"""
+class Solution(object):
+    def nextPermutation(self, nums):
+        for i in range(len(nums)-2, -1, -1):
+            if nums[i] < nums[i+1]:
+                break
+        else:
+            nums.reverse()
+            return
+        for j in range(len(nums)-1, i, -1):
+            if nums[j] > nums[i]:
+                nums[i], nums[j] = nums[j], nums[i]
+                break
+        for j in range(0, (len(nums) - i)//2):
+            nums[i+j+1], nums[len(nums)-j-1] = nums[len(nums)-j-1], nums[i+j+1]
