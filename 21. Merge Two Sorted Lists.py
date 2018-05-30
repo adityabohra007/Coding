de046884ef59c01ad99cb891a8aee9f91f0ce3cc
KAILIN SHI <kailinshi19809@gmail.com>
2018-05-30 02:11:35 -0700

    21. Merge Two Sorted Lists

---
+++ 21. Merge Two Sorted Lists.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        heap = []
+        dummy = ListNode(-1)
+        head = dummy
+        if l1:
+            heapq.heappush(heap, (l1.val, l1))
+        if l2:
+            heapq.heappush(heap, (l2.val, l2))
+        while len(heap) > 0:
+            val, node = heapq.heappop(heap)
+            if node.next:
+                heapq.heappush(heap, (node.next.val, node.next))
+            head.next = node
+            head = head.next
+        return dummy.next
